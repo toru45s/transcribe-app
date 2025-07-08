41ec3a24588cec04b6c86639f13c59363bf7c8b3
Tohru Yaginuma <toru45s@gmail.com>
2025-07-07 17:42:03 -0700

    update audio recording init

---
+++ frontend/src/hooks/audio-recording.tsx
@@ -0,0 +1,99 @@
+"use client";
+import { useState, useEffect, useRef } from "react";
+
+export const useAudioStream = () => {
+  const [isRecording, setIsRecording] = useState(false);
+  const [audioStream, setAudioStream] = useState<MediaStream | null>(null);
+  const [mediaRecorder, setMediaRecorder] = useState<MediaRecorder | null>(
+    null
+  );
+  const [audioBlob, setAudioBlob] = useState<Blob | null>(null);
+  const [recordingTime, setRecordingTime] = useState(0);
+  const timerRef = useRef<NodeJS.Timeout | null>(null);
+  const RECORDING_MAX_DURATION = 240; // 4 minutes in seconds
+
+  useEffect(() => {
+    if (!audioStream) {
+      navigator.mediaDevices
+        .getUserMedia({ audio: true })
+        .then((stream: MediaStream) => {
+          setAudioStream(stream);
+          const mediaRecorder = new MediaRecorder(stream);
+          setMediaRecorder(mediaRecorder);
+          let audio: BlobPart[] = [];
+
+          mediaRecorder.ondataavailable = (event) => {
+            if (event.data.size > 0) {
+              audio = [event.data];
+            }
+          };
+
+          mediaRecorder.onstop = () => {
+            const b = new Blob(audio, { type: "audio/wav" });
+            setAudioBlob(b as Blob);
+            console.log("audioBlob", b);
+          };
+        })
+        .catch((error) => {
+          console.error("Error accessing microphone:", error);
+        });
+    }
+
+    return () => {
+      if (timerRef.current) {
+        clearInterval(timerRef.current);
+      }
+    };
+  }, [audioStream]);
+
+  const handleToggleRecording = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    event.preventDefault();
+    if (isRecording) {
+      stopRecording();
+    } else {
+      startRecording();
+    }
+  };
+
+  const startRecording = () => {
+    mediaRecorder?.start();
+    setIsRecording(true);
+    setRecordingTime(0);
+    setAudioBlob(null);
+    timerRef.current = setInterval(() => {
+      setRecordingTime((prevTime) => {
+        if (prevTime >= RECORDING_MAX_DURATION - 1) {
+          stopRecording();
+          return RECORDING_MAX_DURATION;
+        }
+        return prevTime + 1;
+      });
+    }, 1000);
+  };
+
+  const stopRecording = () => {
+    mediaRecorder?.stop();
+    setIsRecording(false);
+    if (timerRef.current) {
+      clearInterval(timerRef.current);
+    }
+  };
+
+  const formatTime = (seconds: number) => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+
+    return `${minutes.toString().padStart(2, "0")}:${remainingSeconds
+      .toString()
+      .padStart(2, "0")}`;
+  };
+  return {
+    isRecording,
+    handleToggleRecording,
+    formatTime,
+    audioBlob,
+    recordingTime,
+  };
+};
